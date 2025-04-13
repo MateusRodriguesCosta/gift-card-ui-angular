891c0151f90b59cc9cea6f278a25d711f38eb6b4
mateus.costa <mateusespindola25@hotmail.com>
2025-04-12 21:37:20 -0300

    chore:update project name title.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'incomm-poc'`, () => {
+  it(`should have as title 'gift-card-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('incomm-poc');
+    expect(app.title).toEqual('gift-card-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, incomm-poc');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gift-card-ui');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'incomm-poc';
+  title = 'gift-card-ui';
 }
