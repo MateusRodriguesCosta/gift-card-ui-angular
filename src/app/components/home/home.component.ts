935fa0428344a2eeacbe41edac17c64ab6554be1
mateus.costa <mateusespindola25@hotmail.com>
2025-05-08 21:31:29 -0300

    feat:add p-drawer as a side menu.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,26 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { GiftCardTransactionsComponent } from '../dashboard/gift-card-transactions/gift-card-transactions.component';
 import { GiftCardOverviewComponent } from '../dashboard/gift-card-overview/gift-card-overview.component';
 import { AuthenticationService } from '../../security/services/authentication.service';
 import { AuthenticationTokenService } from '../../security/services/authentication-token.service';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Menu } from 'primeng/menu';
+import { Drawer } from 'primeng/drawer';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   imports: [
-    GiftCardTransactionsComponent,
     GiftCardOverviewComponent,
-    RouterOutlet
+    RouterOutlet,
+    Menu,
+    Drawer,
+    Button
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
 
+  public visible = true;
+  protected menuItems: MenuItem [] = [
+    { label: 'New', icon: 'pi pi-plus' },
+    { label: 'Search', icon: 'pi pi-search' }
+  ];
+
   constructor(protected authenticationService: AuthenticationService, protected authenticationTokenService: AuthenticationTokenService) {}
 
   ngOnInit() {
-    if (!this.authenticationTokenService.accessToken) this.authenticationService.login("user", "user123").subscribe();
+    if (!this.authenticationTokenService.isLoggedIn) this.authenticationService.login("user", "user123").subscribe();
   }
 
 }
